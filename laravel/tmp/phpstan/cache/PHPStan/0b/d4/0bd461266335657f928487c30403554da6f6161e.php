e3ec84d812c08fd0a7da2cd0afd6ac6822f15456
marco76tv <marco.sottana@gmail.com>
2025-01-14 13:36:10 +0100

    up

---
+++ laravel/tmp/phpstan/cache/PHPStan/0b/d4/0bd461266335657f928487c30403554da6f6161e.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+// odsl-F:/var/www/techplanner/laravel/vendor/composer/../tecnickcom/tcpdf/config
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    'F:\\var\\www\\techplanner\\laravel\\vendor\\tecnickcom\\tcpdf\\config\\tcpdf_config.php' => 
+    array (
+      0 => '5ae05e0b837f761ab3587e26fbd335bb76ae8b30',
+      1 => 
+      array (
+      ),
+      2 => 
+      array (
+      ),
+      3 => 
+      array (
+        0 => 'K_BLANK_IMAGE',
+        1 => 'PDF_PAGE_FORMAT',
+        2 => 'PDF_PAGE_ORIENTATION',
+        3 => 'PDF_CREATOR',
+        4 => 'PDF_AUTHOR',
+        5 => 'PDF_HEADER_TITLE',
+        6 => 'PDF_HEADER_STRING',
+        7 => 'PDF_UNIT',
+        8 => 'PDF_MARGIN_HEADER',
+        9 => 'PDF_MARGIN_FOOTER',
+        10 => 'PDF_MARGIN_TOP',
+        11 => 'PDF_MARGIN_BOTTOM',
+        12 => 'PDF_MARGIN_LEFT',
+        13 => 'PDF_MARGIN_RIGHT',
+        14 => 'PDF_FONT_NAME_MAIN',
+        15 => 'PDF_FONT_SIZE_MAIN',
+        16 => 'PDF_FONT_NAME_DATA',
+        17 => 'PDF_FONT_SIZE_DATA',
+        18 => 'PDF_FONT_MONOSPACED',
+        19 => 'PDF_IMAGE_SCALE_RATIO',
+        20 => 'HEAD_MAGNIFICATION',
+        21 => 'K_CELL_HEIGHT_RATIO',
+        22 => 'K_TITLE_MAGNIFICATION',
+        23 => 'K_SMALL_RATIO',
+        24 => 'K_THAI_TOPCHARS',
+        25 => 'K_TCPDF_CALLS_IN_HTML',
+        26 => 'K_ALLOWED_TCPDF_TAGS',
+        27 => 'K_TCPDF_THROW_EXCEPTION_ERROR',
+        28 => 'K_TIMEZONE',
+      ),
+    ),
+  ),
+));
